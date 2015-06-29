a430e53f005b45da1b244d1283120aaa860e6c8d
Tibor Simko <tibor.simko@cern.ch>
2015-06-29 12:04:28 +0200

    travis: fix pep257-related build failure
    * Fixes Travis CI build process by removing flask-menu left-overs in
      pep257 testing.  At the same time, (i) leaves pep257 testing commented
      for now, due to very large number of missing docstrings in
      contrib.marc21; (ii) leaves sphinx testing commented for now.
    
    * Amends predated copyright statements.
    
    Signed-off-by: Tibor Simko <tibor.simko@cern.ch>

--- run-tests.sh
+++ run-tests.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 #
 # This file is part of DoJSON
-# Copyright (C) 2013, 2014 CERN.
+# Copyright (C) 2015 CERN.
 #
 # DoJSON is free software; you can redistribute it and/or modify
 # it under the terms of the Revised BSD License; see LICENSE file for
 # more details.
 
-pep257 flask_menu && \
+# FIXME contrib.marc21 has tons of undefined docstrings
+#pep257 dojson && \
 sphinx-build -qnNW docs docs/_build/html && \
 python setup.py test
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of DoJSON
-# Copyright (C) 2013, 2014, 2015 CERN.
+# Copyright (C) 2015 CERN.
 #
 # DoJSON is free software; you can redistribute it and/or modify
 # it under the terms of the Revised BSD License; see LICENSE file for
