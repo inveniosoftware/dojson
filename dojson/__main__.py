19e2af9db80ac742c1a9e7ec464b610f40a796f4
Jiri Kuncar <jiri.kuncar@cern.ch>
2016-07-14 08:56:01 +0200

    api: support for python -m dojson
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

---
+++ dojson/__main__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of DoJSON
+# Copyright (C) 2016 CERN.
+#
+# DoJSON is free software; you can redistribute it and/or
+# modify it under the terms of the Revised BSD License; see LICENSE
+# file for more details.
+
+"""DoJSON is a simple Pythonic JSON to JSON converter."""
+
+from dojson.cli import cli
+
+if __name__ == '__main__':
+    cli()
