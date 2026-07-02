f9a832ff45eb3e19bfb5cef8fdcb502bafa4c02f
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-07-02 18:00:48 +0200

    release: v1.7.1

--- dojson/version.py
+++ dojson/version.py
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: 2015, 2016, 2017 CERN.
 # SPDX-FileCopyrightText: 2025 Graz University of Technology.
+# SPDX-FileCopyrightText: 2026 TU Wien.
 # SPDX-License-Identifier: BSD-3-Clause
 
 """Version information for DoJSON.
@@ -11,4 +12,4 @@ This file is imported by ``dojson.__init__``, and parsed by
 # Do not change the format of this next line. Doing so risks breaking
 # setup.py and docs/conf.py
 
-__version__ = "1.7.0"
+__version__ = "1.7.1"
